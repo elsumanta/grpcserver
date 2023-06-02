8998143580da5a5a4b090e3215ec7fd78871dbef
elimsaragih <elim.saragih@tokopedia.com>
2023-06-02 14:30:54 +0700

    init grpc server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,10 @@
 module github.com/grpcserver/server
 
 go 1.16
+
+require (
+	github.com/elsumanta/grpcserver v0.0.0-20230602070103-9efa3b97b08c
+	golang.org/x/net v0.10.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0
+)
--- server/main.go
+++ server/main.go
@@ -7,8 +7,9 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/grpcserver/grpc"
+	pb "github.com/elsumanta/grpcserver/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
@@ -31,6 +32,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
+	reflection.Register(s)
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
